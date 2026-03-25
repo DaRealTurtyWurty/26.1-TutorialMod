e3c6713e74a932902a9087623e1782861b64e822
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2026-03-25 14:56:32 +0000

    Initial commit

---
+++ common/src/main/java/dev/turtywurty/tutorialmod/Constants.java
@@ -0,0 +1,10 @@
+package dev.turtywurty.tutorialmod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+	public static final String MOD_ID = "tutorialmod";
+	public static final String MOD_NAME = "Tutorial Mod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
