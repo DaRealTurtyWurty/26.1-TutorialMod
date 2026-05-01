e23858e714178c272adcc16c316827a4cec9aa49
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2026-05-01 15:17:49 +0100

    Tameable Entity Tutorial

--- common/src/main/java/dev/turtywurty/tutorialmod/entities/ExampleEntity.java
+++ common/src/main/java/dev/turtywurty/tutorialmod/entities/ExampleEntity.java
@@ -1,10 +1,11 @@
 package dev.turtywurty.tutorialmod.entities;
 
-import dev.turtywurty.tutorialmod.init.ModEntityTypes;
 import dev.turtywurty.tutorialmod.init.ModItems;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -15,7 +16,7 @@ import net.minecraft.world.level.Level;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
-public class ExampleEntity extends Animal {
+public class ExampleEntity extends TamableAnimal {
     public ExampleEntity(EntityType<? extends ExampleEntity> type, Level level) {
         super(type, level);
     }
@@ -29,7 +30,9 @@ public class ExampleEntity extends Animal {
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(3, new PanicGoal(this, 1.25D));
+        this.goalSelector.addGoal(3, new TamableAnimalPanicGoal(1.25D, DamageTypeTags.PANIC_ENVIRONMENTAL_CAUSES));
+        this.goalSelector.addGoal(4, new SitWhenOrderedToGoal(this));
+        this.goalSelector.addGoal(5, new FollowOwnerGoal(this, 1.0D, 10, 2));
         this.goalSelector.addGoal(6, new RandomStrollGoal(this, 1.0D, 120, false));
         this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 10.0F));
         this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
@@ -37,11 +40,51 @@ public class ExampleEntity extends Animal {
 
     @Override
     public boolean isFood(@NonNull ItemStack itemStack) {
-        return itemStack.is(ModItems.EXAMPLE_ITEM.get());
+        return false;
     }
 
     @Override
     public @Nullable AgeableMob getBreedOffspring(@NonNull ServerLevel serverLevel, @NonNull AgeableMob ageableMob) {
-        return new ExampleEntity(ModEntityTypes.EXAMPLE_ENTITY.get(), serverLevel);
+        return null;
+    }
+
+    @Override
+    public @NonNull InteractionResult mobInteract(@NonNull Player player, @NonNull InteractionHand hand) {
+        ItemStack itemStack = player.getItemInHand(hand);
+        if (!level().isClientSide()) {
+            if (!isTame() && itemStack.is(ModItems.EXAMPLE_FOOD.get()) && !isBaby()) {
+                itemStack.consume(1, player);
+
+                tame(player);
+                this.navigation.stop();
+                setTarget(null);
+                setOrderedToSit(true);
+                level().broadcastEntityEvent(this, EntityEvent.TAMING_SUCCEEDED);
+                return InteractionResult.SUCCESS;
+            } else if (isTame()) {
+                InteractionResult result = super.mobInteract(player, hand);
+                if (result.consumesAction() || !isOwnedBy(player))
+                    return result;
+
+                setOrderedToSit(!isOrderedToSit());
+                this.jumping = false;
+                this.navigation.stop();
+                setTarget(null);
+                return InteractionResult.SUCCESS;
+            }
+        }
+
+        return isOwnedBy(player) || isTame() || itemStack.is(ModItems.EXAMPLE_FOOD.get())
+                ? InteractionResult.CONSUME
+                : InteractionResult.PASS;
+    }
+
+    @Override
+    protected void applyTamingSideEffects() {
+        super.applyTamingSideEffects();
+
+        if (!level().isClientSide()) {
+            EntityType.PIG.spawn((ServerLevel) level(), blockPosition(), EntitySpawnReason.SPAWN_ITEM_USE);
+        }
     }
 }
